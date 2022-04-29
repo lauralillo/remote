1612281bf52902223df18ac7262f03d52636abac
Laura Lillo Collado <llilcol@alumno.upv.es>
2022-04-29 10:48:24 +0200

    Initial Commit

---
+++ pru.py
@@ -0,0 +1,8 @@
+import math
+
+x = float(input('Escribe la coordenada X del punto: '))
+y = float(input('Escribe la coordenada Y del punto: '))
+m = float(input('Escribe la pendiente de la recta: '))
+b = float(input('Escribe el término independiente de la recta: '))
+d = abs((m * x - y + b)) / (math.sqrt(m ** 2 + 1))
+print('La distancia del punto a la recta es: ',d)
